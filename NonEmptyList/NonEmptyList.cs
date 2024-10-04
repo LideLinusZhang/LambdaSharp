d003ca3974b88195356718d090bcf94747011ad3
Shuyao Shi <foreverstar717@gmail.com>
2024-10-04 16:17:33 -0400

    Implemented NonEmptyList

---
+++ NonEmptyList/NonEmptyList.cs
@@ -0,0 +1,42 @@
+﻿using LambdaSharp.Maybe;
+using LambdaSharp.Tuple;
+
+namespace LambdaSharp.NotEmptyList
+{
+    internal class NonEmptyList<T>
+    {
+        private T[] Array;
+
+        internal NonEmptyList(T[] array)
+        {
+            if (array == null || array.Length == 0) throw new ArgumentException();
+
+            Array = array;
+        }
+
+
+        public static IMaybe<NonEmptyList<T>> FromArray(T[] array) 
+        {
+            if (array == null || array.Length == 0) return Maybe.Maybe.Nothing<NonEmptyList<T>>();
+
+            return new Just<NonEmptyList<T>>(new NonEmptyList<T>(array));
+        }
+
+        public static NonEmptyList<object> FromTuple(Tuple.Tuple<object, object> tuple) => new NonEmptyList<object>(tuple.ToArray());
+
+        public static bool IsNonEmpty() => true;
+
+        public static NonEmptyList<T> UnsafeCoerce(T[] array)
+        {
+            if (array == null || array.Length == 0) throw new ArgumentException("NonEmptyList#unsafeCoerce was ran on an empty array");
+
+            return new NonEmptyList<T>(array);
+        }
+
+        public static T Head(NonEmptyList<T> a) => a.Array[0];
+
+        public static T Last(NonEmptyList<T> a) => a.Array.Last();
+
+        public static T[] Tail(NonEmptyList<T> a) => a.Array[1..];
+    }
+}
