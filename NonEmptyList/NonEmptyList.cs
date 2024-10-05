f23ffdfe14f3f80a06eaf5478424a41742ca5dd2
Shuyao Shi <foreverstar717@gmail.com>
2024-10-05 11:55:24 -0400

    NonEmptyList now implement the interface IList<T>

--- NonEmptyList/NonEmptyList.cs
+++ NonEmptyList/NonEmptyList.cs
@@ -1,23 +1,30 @@
 ﻿using LambdaSharp.Maybe;
 using LambdaSharp.Tuple;
+using System.Collections;
 
 namespace LambdaSharp.NotEmptyList
 {
-    internal class NonEmptyList<T>
+    internal class NonEmptyList<T> : IList<T>
     {
-        private T[] Array;
+        private readonly List<T> list;
 
-        internal NonEmptyList(T[] array)
+        public int Count => ((ICollection<T>)list).Count;
+
+        public bool IsReadOnly => ((ICollection<T>)list).IsReadOnly;
+
+        public T this[int index] { get => ((IList<T>)list)[index]; set => ((IList<T>)list)[index] = value; }
+
+        public NonEmptyList(IList<T> list)
         {
-            if (array == null || array.Length == 0) throw new ArgumentException();
+            if (list is null || list.Count == 0) throw new ArgumentException();
 
-            Array = array;
+            this.list = list.ToList();
         }
 
 
-        public static IMaybe<NonEmptyList<T>> FromArray(T[] array) 
+        public static IMaybe<NonEmptyList<T>> FromArray(IList<T> array)
         {
-            if (array == null || array.Length == 0) return Maybe.Maybe.Nothing<NonEmptyList<T>>();
+            if (array == null || array.Count == 0) return Maybe.Maybe.Nothing<NonEmptyList<T>>();
 
             return new Just<NonEmptyList<T>>(new NonEmptyList<T>(array));
         }
@@ -26,17 +33,67 @@ namespace LambdaSharp.NotEmptyList
 
         public static bool IsNonEmpty() => true;
 
-        public static NonEmptyList<T> UnsafeCoerce(T[] array)
+        public static NonEmptyList<T> UnsafeCoerce(IList<T> array)
         {
-            if (array == null || array.Length == 0) throw new ArgumentException("NonEmptyList#unsafeCoerce was ran on an empty array");
+            if (array == null || array.Count == 0) throw new ArgumentException("NonEmptyList#unsafeCoerce was ran on an empty array");
 
             return new NonEmptyList<T>(array);
         }
 
-        public static T Head(NonEmptyList<T> a) => a.Array[0];
+        public static T Head(NonEmptyList<T> a) => a[0];
 
-        public static T Last(NonEmptyList<T> a) => a.Array.Last();
+        public static T Last(NonEmptyList<T> a) => a.Last();
 
-        public static T[] Tail(NonEmptyList<T> a) => a.Array[1..];
+        public static IList<T> Tail(NonEmptyList<T> a) => a.list[1..];
+
+        public int IndexOf(T item)
+        {
+            return ((IList<T>)list).IndexOf(item);
+        }
+
+        public void Insert(int index, T item)
+        {
+            ((IList<T>)list).Insert(index, item);
+        }
+
+        public void RemoveAt(int index)
+        {
+            ((IList<T>)list).RemoveAt(index);
+        }
+
+        public void Add(T item)
+        {
+            ((ICollection<T>)list).Add(item);
+        }
+
+        public void Clear()
+        {
+            ((ICollection<T>)list).Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return ((ICollection<T>)list).Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            ((ICollection<T>)list).CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(T item)
+        {
+            return ((ICollection<T>)list).Remove(item);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)list).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)list).GetEnumerator();
+        }
     }
 }
