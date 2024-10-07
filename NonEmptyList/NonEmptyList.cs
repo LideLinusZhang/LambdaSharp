4840d33d288309e72e935d72fd4f3c70f76ce2ba
Shuyao Shi <foreverstar717@gmail.com>
2024-10-07 12:12:42 -0400

    NonEmptyList now implment IList interface

--- NonEmptyList/NonEmptyList.cs
+++ NonEmptyList/NonEmptyList.cs
@@ -8,57 +8,11 @@ namespace LambdaSharp.NotEmptyList
     {
         private readonly List<T> list;
 
-        public int Count => ((ICollection<T>)list).Count;
-
-        public bool IsReadOnly => ((ICollection<T>)list).IsReadOnly;
-
-        public T this[int index] { get => ((IList<T>)list)[index]; set => ((IList<T>)list)[index] = value; }
-
         public NonEmptyList(IList<T> list)
         {
             if (list is null || list.Count == 0) throw new ArgumentException();
 
             this.list = list.ToList();
-        }
-
-
-        public static IMaybe<NonEmptyList<T>> FromArray(IList<T> array)
-        {
-            if (array == null || array.Count == 0) return Maybe.Maybe.Nothing<NonEmptyList<T>>();
-
-            return new Just<NonEmptyList<T>>(new NonEmptyList<T>(array));
-        }
-
-        public static NonEmptyList<object> FromTuple(Tuple.Tuple<object, object> tuple) => new NonEmptyList<object>(tuple.ToArray());
-
-        public static bool IsNonEmpty() => true;
-
-        public static NonEmptyList<T> UnsafeCoerce(IList<T> array)
-        {
-            if (array == null || array.Count == 0) throw new ArgumentException("NonEmptyList#unsafeCoerce was ran on an empty array");
-
-            return new NonEmptyList<T>(array);
-        }
-
-        public static T Head(NonEmptyList<T> a) => a[0];
-
-        public static T Last(NonEmptyList<T> a) => a.Last();
-
-        public static IList<T> Tail(NonEmptyList<T> a) => a.list[1..];
-
-        public int IndexOf(T item)
-        {
-            return ((IList<T>)list).IndexOf(item);
-        }
-
-        public void Insert(int index, T item)
-        {
-            ((IList<T>)list).Insert(index, item);
-        }
-
-        public void RemoveAt(int index)
-        {
-            ((IList<T>)list).RemoveAt(index);
         }
 
         public void Add(T item)
@@ -80,11 +34,16 @@ namespace LambdaSharp.NotEmptyList
         {
             ((ICollection<T>)list).CopyTo(array, arrayIndex);
         }
+        public int Count => ((ICollection<T>)list).Count;
 
-        public bool Remove(T item)
+        public static IMaybe<NonEmptyList<T>> FromArray(IList<T> array)
         {
-            return ((ICollection<T>)list).Remove(item);
+            if (array == null || array.Count == 0) return Maybe.Maybe.Nothing<NonEmptyList<T>>();
+
+            return new Just<NonEmptyList<T>>(new NonEmptyList<T>(array));
         }
+
+        public static NonEmptyList<object> FromTuple(Tuple.Tuple<object, object> tuple) => new NonEmptyList<object>(tuple.ToArray());
 
         public IEnumerator<T> GetEnumerator()
         {
@@ -95,5 +54,44 @@ namespace LambdaSharp.NotEmptyList
         {
             return ((IEnumerable)list).GetEnumerator();
         }
+
+        public static T Head(NonEmptyList<T> a) => a[0];
+
+        public int IndexOf(T item)
+        {
+            return ((IList<T>)list).IndexOf(item);
+        }
+
+        public void Insert(int index, T item)
+        {
+            ((IList<T>)list).Insert(index, item);
+        }
+
+        public static bool IsNonEmpty() => true;
+
+        public bool IsReadOnly => ((ICollection<T>)list).IsReadOnly;
+
+        public static T Last(NonEmptyList<T> a) => a.Last();
+
+        public void RemoveAt(int index)
+        {
+            ((IList<T>)list).RemoveAt(index);
+        }
+
+        public bool Remove(T item)
+        {
+            return ((ICollection<T>)list).Remove(item);
+        }
+
+        public static IList<T> Tail(NonEmptyList<T> a) => a.list[1..];
+
+        public static NonEmptyList<T> UnsafeCoerce(IList<T> array)
+        {
+            if (array == null || array.Count == 0) throw new ArgumentException("NonEmptyList#unsafeCoerce was ran on an empty array");
+
+            return new NonEmptyList<T>(array);
+        }
+
+        public T this[int index] { get => ((IList<T>)list)[index]; set => ((IList<T>)list)[index] = value; }
     }
 }
